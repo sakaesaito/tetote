b287089cb4487b5d0498ec24c72dc41b620e1cee
sakae .s <user@s3110MacBook-Air.local>
2025-07-22 07:34:52 +0900

    “（7/21個別スタップページ全員分）”

--- footer.php
+++ footer.php
@@ -8,40 +8,40 @@
                     あなたも会社も一緒に成長していきましょう。
                 </p>
                 <div class="button-viewmore-entry">
-                    <a href="<?php echo home_url('/entry/entry.html'); ?>">ENTRY</a>
+                    <a href="<?php echo esc_url(home_url('/entry/')); ?>">ENTRY</a>
                 </div>
             </div>
-            <div class="content-wrapper footer-content-wrapper">
+            <div class="content-wrapper footer-content-wrapper"> 
                 <div class="footer-links">
                     <div class="footer-list">
                         <div class="footer-list1">
-                            <a href="<?php echo home_url('/'); ?>">ホーム</a>
-                            <a href="#about">TETOTEについて</a>
-                            <a href="#staff">人を知る</a>
-                            <a href="#career">研修制度とキャリアパス</a>
-                            <a href="#benefits">福利厚生</a>
+                            <a href="<?php echo esc_url(home_url('/')); ?>">ホーム</a>
+                            <a href="<?php echo esc_url(home_url('/about/')); ?>">TETOTEについて</a>
+                            <a href="<?php echo esc_url(home_url('/staff/')); ?>">人を知る</a>
+                            <a href="<?php echo esc_url(home_url('/career/')); ?>">研修制度とキャリアパス</a>
+                            <a href="<?php echo esc_url(home_url('/benefits/')); ?>">福利厚生</a>
                         </div>
                         <div class="footer-list2">
-                            <a href="#blog">採用ブログ</a>
-                            <a href="<?php echo home_url('/details/details.html'); ?>">募集要項</a>
-                            <a href="#">よくある質問</a>
-                            <a href="#">会社概要</a>
+                            <a href="<?php echo esc_url(home_url('/blog-detail/')); ?>">採用ブログ</a>
+                            <a href="<?php echo esc_url(home_url('/details/')); ?>">募集要項</a>
+                            <a href="<?php echo esc_url(home_url('/faq/')); ?>">よくある質問</a>
+                            <a href="<?php echo esc_url(home_url('/about/')); ?>">会社概要</a>
                         </div>
                     </div>
                     <div class="footer-logo">
-                        <a href="<?php echo home_url(); ?>">
+                        <a href="<?php echo esc_url(home_url('/')); ?>">
                             <img src="<?php echo get_template_directory_uri(); ?>/img/tetote-logo-black.png" alt="会社ロゴ">
                         </a>
                     </div>
                 </div>
             </div>
-
+            
             <ul class="footer-nav">
-                <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook-icon.png" alt="Facebook"></a></li>
-                <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter-icon.png" alt="Twitter"></a></li>
-                <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/movie-icon.png" alt="Movie"></a></li>
+                <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook-icon.png" alt="フェイスブックアイコン"></a></li>
+                <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/twitter-icon.png" alt="ツイッターアイコン"></a></li>
+                <li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/movie-icon.png" alt="ユーチューブアイコン"></a></li>
             </ul>
-            <small class="copyright">&copy;<?php echo date("Y"); ?> TETOTE All Right Reserved.</small>
+            <small class="copyright">&copy;2024 TETOTE All Right Reserved.</small>
         </div>
     </footer>
 
--- header.php
+++ header.php
@@ -2,21 +2,20 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?></title>
     <meta name="robots" content="noindex">
+    <meta name="description" content="テクノロジーで社会課題を解決する。AIやビッグデータ分析などの技術を活用した社会課題解決サービスを提供するTETOTEの採用サイト。">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/tetote-favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Viga&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<?php wp_head(); ?>    
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&family=Poppins&family=Viga&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css">
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -24,16 +23,16 @@
         <div class="header-inner">
             <div class="header-nav">
                 <h1 class="header-logo">
-                    <a class="header-logo__link" href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/tetote-logo-white 1.png" alt=""></a>
+                    <a class="header-logo__link" href="<?php echo esc_url(home_url('/')); ?>">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/tetote-logo-white 1.png" alt="会社ロゴ">
+                    </a>
                 </h1>
                 <div class="header-link">
-                    <a class="header-button header-button-recruit" href="<?php echo home_url('/details/details.html'); ?>">募集要項</a>
-                    <a class="header-button header-button-entry" href="<?php echo home_url('/entry/entry.html'); ?>">ENTRY</a>
+                    <a class="header-button header-button-recruit" href="<?php echo esc_url(home_url('/details/')); ?>">募集要項</a>
+                    <a class="header-button header-button-entry" href="<?php echo esc_url(home_url('/entry/')); ?>">ENTRY</a>
                     <button class="header-hamburger js-hamburger">
                         <div class="header-hamburger-icon">
-                            <span></span>
-                            <span></span>
-                            <span></span>
+                            <span></span><span></span><span></span>
                             <div class="header-hamburger-item">menu</div>
                         </div>
                     </button>
@@ -41,25 +40,27 @@
                 <nav class="drawer-nav js-drawer">
                     <div class="drawer-inner">
                         <div class="drawer-left">
-                            <a class="drawer-logo-link" href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/tetote-logo-black.png"></a>
+                            <a class="drawer-logo-link" href="<?php echo esc_url(home_url('/')); ?>">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/tetote-logo-black.png" alt="会社ロゴ">
+                            </a>
                         </div>
                         <div class="drawer-right">
                             <div class="drawer-list">
                                 <div class="drawer-line">
-                                    <div class="drawer-item"><a href="#">ABOUT US<br><span>TETOTEについて</span></a></div>
-                                    <div class="drawer-item"><a href="#">STAFF<br><span>写真について</span></a></div>
-                                    <div class="drawer-item"><a href="#">BLOG<br><span>採用ブログ</span></a></div>
+                                    <div class="drawer-item"><a href="<?php echo esc_url(home_url('/about/')); ?>">ABOUT US<br><span>TETOTEについて</span></a></div>
+                                    <div class="drawer-item"><a href="<?php echo esc_url(home_url('/staff/')); ?>">STAFF<br><span>社員について</span></a></div>
+                                    <div class="drawer-item"><a href="<?php echo esc_url(home_url('/blog/')); ?>">BLOG<br><span>採用ブログ</span></a></div>
                                     <div><p class="drawer-parts"></p></div>
                                 </div>
                                 <div>
-                                    <div class="drawer-item"><a href="#">BENEFITS<br><span>福利厚生について</span></a></div>
-                                    <div class="drawer-item"><a href="#">CARRER<br><span>研修制度とキャリアパス</span></a></div>
-                                    <div class="drawer-item"><a href="#">FAQ<br><span>よくある質問</span></a></div>
+                                    <div class="drawer-item"><a href="<?php echo esc_url(home_url('/benefits/')); ?>">BENEFITS<br><span>福利厚生について</span></a></div>
+                                    <div class="drawer-item"><a href="<?php echo esc_url(home_url('/career/')); ?>">CARRER<br><span>研修制度とキャリアパス</span></a></div> 
+                                    <div class="drawer-item"><a href="<?php echo esc_url(home_url('/faq/')); ?>">FAQ<br><span>よくある質問</span></a></div>
                                 </div>
                             </div>                 
                             <div class="drawer-button">
-                                <a class="drawer-button-recruit" href="<?php echo home_url('/details/details.html'); ?>">募集要項</a>
-                                <a class="drawer-button-entry" href="<?php echo home_url('/entry/entry.html'); ?>">ENTRY</a>
+                                <a class="drawer-button-recruit" href="<?php echo esc_url(home_url('/details/')); ?>">募集要項</a>
+                                <a class="drawer-button-entry" href="<?php echo esc_url(home_url('/entry/')); ?>">ENTRY</a>
                             </div>
                         </div>
                     </div>
